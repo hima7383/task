98653f6e1a336aba36cfc6e1fdb00d5671bce85b
ibrahim mohamed <85079416+hima7383@users.noreply.github.com>
2022-03-31 23:16:56 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,40 +1,98 @@
 #include <bits/stdc++.h>
-#include<ctime>
+#define far(i,b,c) for(int i = b ; i <c;i++)
 using namespace std;
+
+
+int bin_serch(int arr[],int l,int h,int key)
+{
+    int m= (l+h)/2;
+    while(l<=h)
+    {
+        if(arr[m]==key)
+        {
+            return m;
+        }
+        if(arr[m]>key)
+        {
+            m=h-1;
+        }
+        else
+        {
+            m=l+1;
+        }
+
+
+
+
+    }
+
+}
+void remove(std::vector<int> &v)
+{
+    auto end = v.end();
+    for (auto it = v.begin(); it != end; ++it) {
+        end = std::remove(it + 1, end, *it);
+    }
+
+    v.erase(end, v.end());
+}
+
+
 int main(){
 
-srand(time(NULL));
-int s = rand()%100;
-int flag=0;
-while(flag<=8)
-{
-    if(flag==0){cout<<"guese the number "<<endl<<"----------------------------------"<<endl;}
 
-int x;
+int test;
+cin>>test;
+while(test--)
+{
+string x;
 cin>>x;
-if(x >s)
+vector <char> jj(26);
+map <char,int> freq;
+int flag=0;
+for(int i = 0 ; i <x.size();i++)
 {
-    cout<<"too high try again"<<endl<<"***************"<<endl;
-    flag++;
+    jj[i]=x[i];
 }
-else if(x<s)
+for(int i = 0 ; i <x.size();i++)
 {
-    cout<<"too low try again"<<endl<<"****************"<<endl;
-    flag--;
+    if(jj[i]==jj[i+1]){
+        jj.erase(jj.begin()+i);
+         jj.erase(jj.begin()+(i+1));
+    }
 }
-else
+for(auto x:jj)
 {
-    cout<<"you found it!"<<endl;
-    break;
+    cout<<x;
+}
+for(int i = 0 ; i <jj.size();i++)
+{
+    freq[jj[i]]++;
+}
+for(int i = 0 ; i <jj.size();i++)
+{
+    cout<<freq[i]<<" ";
+}
+
+for(int i = 0 ; i <jj.size();i++)
+{
+    if (freq[jj[i]]%2==0){flag++;};
+}
+
+cout<<flag<<endl;
+
+
+
+
+
+
 }
 
 
 
 
-
-
-
+return 0;
 
 }
 
-}
+
